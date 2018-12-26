3e99f7150810180302817dc515adbb47a1d44ca9
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-12-26 19:47:07 -0200

    JSON with Go

---
+++ source/golang/json-sample.go
@@ -0,0 +1,34 @@
+//  $ curl -s -XPOST -d'{"firstname":"Donald","lastname":"Trump","age":70}' http://localhost:8080/decode
+//  $ curl -s http://localhost:8080/encode
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type User struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Age       int    `json:"age"`
+}
+
+func main() {
+	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		json.NewDecoder(r.Body).Decode(&user)
+		fmt.Fprintf(w, "%s %s is %d years old!", user.Firstname, user.Lastname, user.Age)
+	})
+
+	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
+		peter := User{
+			Firstname: "John",
+			Lastname:  "Doe",
+			Age:       25,
+		}
+		json.NewEncoder(w).Encode(peter)
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
