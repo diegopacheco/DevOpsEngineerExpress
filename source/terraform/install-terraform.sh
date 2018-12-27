6b633fbe1daa3a4a8e2cfe2ac8e5e88259e10fd7
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-12-27 18:30:33 -0200

    Install Terraform script

---
+++ source/terraform/install-terraform.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
+unzip terraform_0.11.11_linux_amd64.zip
+rm -rf terraform_0.11.11_linux_amd64.zip
+./terraform
