c7ea7bf5e9609fdd64f786bf74df1cf3704d32e5
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-12-27 19:06:25 -0200

    Script to run jenkins 2 on docker

---
+++ source/jenkins/run-jenkins-docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "GOTO: http://localhost:8282/jenkins"
+
+docker stop jenkins; docker rm jenkins;
+
+docker run --network=host --name jenkins -p 8282:8080 -p 50000:50000 -v /var/jenkins/ jenkins
+
