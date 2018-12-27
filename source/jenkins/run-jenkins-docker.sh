7cc018359d150872a4c23cb82527ff18d7095f4c
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-12-27 19:32:44 -0200

    using newer version of jenkins

--- source/jenkins/run-jenkins-docker.sh
+++ source/jenkins/run-jenkins-docker.sh
@@ -4,5 +4,5 @@ echo "GOTO: http://localhost:8282/jenkins"
 
 docker stop jenkins; docker rm jenkins;
 
-docker run --name jenkins -p 8282:8080 -p 50000:50000 -v /var/jenkins/ jenkins
+docker run --name jenkins -p 8282:8080 -p 50000:50000 -v /var/jenkins/ jenkins/jenkins
 
