207955de26be4b2119ee08e4750266d48652158a
Diego Pacheco <diego.pacheco.it@gmail.com>
2018-12-28 14:20:19 -0200

    fix go cass code

--- source/nosql/cassandra/go-cass-sample.go
+++ source/nosql/cassandra/go-cass-sample.go
@@ -8,16 +8,16 @@ import (
 )
 
 func main() {
-	cluster := gocql.NewCluster("127.0.0.1")
-	cluster.Keyspace = "CLUSTER_TEST"
-	cluster.Consistency = gocql.Quorum
+	cluster := gocql.NewCluster("localhost")
+	cluster.Keyspace = "cluster_test"
+	cluster.Consistency = gocql.LocalOne
 	session, _ := cluster.CreateSession()
 	defer session.Close()
 
 	var id string
 	var text string
 
-	iter := session.Query("SELECT * from CLUSTER_TEST.TEST").Iter()
+	iter := session.Query("SELECT * from cluster_test.TEST").Iter()
 	for iter.Scan(&id, &text) {
 		fmt.Println("Data:", id, text)
 	}
